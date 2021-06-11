bbf4e36ed2d667f30fb5081c1e88d49d8ad27317
SFort <github@ssf.tf>
2021-06-11 19:17:32 +0200

    Replace redirects

--- src/main/java/sf/ssf/sfort/mixin/StewItem.java
+++ src/main/java/sf/ssf/sfort/mixin/StewItem.java
@@ -1,23 +1,51 @@
 package sf.ssf.sfort.mixin;
 
-import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.item.MushroomStewItem;
-import net.minecraft.item.SuspiciousStewItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 
-@Mixin(Items.class)
+@Mixin(value = Items.class, priority = 1221)
 public class StewItem{
-	@Redirect(method = "<clinit>",
-			at = @At(value = "NEW", target = "Lnet/minecraft/item/MushroomStewItem;"))
-	private static MushroomStewItem thisStew(Item.Settings settings) {
-		return new MushroomStewItem(settings.maxCount(16));
+	//Here's a little lesson in trickery
+	//mushroom
+	@ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;", ordinal = 0),
+			slice = @Slice(
+					from = @At(value = "NEW", target = "Lnet/minecraft/item/MushroomStewItem;", ordinal = 0),
+					to = @At(value = "NEW", target = "Lnet/minecraft/item/MushroomStewItem;", ordinal = 1)
+			)
+	)
+	private static int new_size_stew0(int i) {
+		return 16;
 	}
-	@Redirect(method = "<clinit>",
-			at = @At(value = "NEW", target = "Lnet/minecraft/item/SuspiciousStewItem;"))
-	private static SuspiciousStewItem thisSuspStew(Item.Settings settings) {
-		return new SuspiciousStewItem(settings.maxCount(16));
+	//rabbit
+	@ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;", ordinal = 0),
+			slice = @Slice(
+					from = @At(value = "NEW", target = "Lnet/minecraft/item/MushroomStewItem;", ordinal = 1),
+					to = @At(value = "NEW", target = "Lnet/minecraft/item/MushroomStewItem;", ordinal = 2)
+			)
+	)
+	private static int new_size_stew1(int i) {
+		return 16;
+	}
+	//beetroot
+	@ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;", ordinal = 0),
+			slice = @Slice(
+					from = @At(value = "NEW", target = "Lnet/minecraft/item/MushroomStewItem;", ordinal = 2),
+					to = @At("TAIL")
+			)
+	)
+	private static int new_size_stew2(int i) {
+		return 16;
+	}
+	@ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxCount(I)Lnet/minecraft/item/Item$Settings;", ordinal = 0),
+			slice = @Slice(
+					from = @At(value = "NEW", target = "Lnet/minecraft/item/SuspiciousStewItem;", ordinal = 0),
+					to = @At("TAIL")
+			)
+	)
+	private static int new_size_stew3(int i) {
+		return 16;
 	}
 }
